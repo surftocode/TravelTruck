fc68b88d30ac835a62e7614c35036502cf8887ca
Merve Delibaşoglu <mervedelibasoglu.dev@gmail.com>
2025-12-22 17:03:39 +0300

    home sayfası cssleri

--- TravelTruck/src/components/Header.jsx
+++ TravelTruck/src/components/Header.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import css from "../styles/Header.module.css";
+import logo from "../assets/icons/Logo.svg";
 export default function Header() {
   return (
-    <div>
-      Travel<span>truck</span>
-      <nav className="header-nav">
-        <Link to="/">Home</Link>
-        <Link to="/catalog">Catalog</Link>
-      </nav>
-    </div>
+    <>
+      <div className="header">
+        <img src={logo} alt="TravelTruck" className={css.logo}/>
+        <nav className={css.header_nav}>
+          <Link to="/">Home</Link>
+          <Link to="/catalog">Catalog</Link>
+        </nav>
+      </div>
+    </>
   );
 }

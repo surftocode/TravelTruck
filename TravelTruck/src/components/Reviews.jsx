36e8b8337e2f92527dc9aa7b802a67aab9252d59
Merve Delibaşoglu <mervedelibasoglu.dev@gmail.com>
2026-01-09 17:59:59 +0300

    reviews css""

--- TravelTruck/src/components/Reviews.jsx
+++ TravelTruck/src/components/Reviews.jsx
@@ -2,21 +2,43 @@ import React from "react";
 import css from "../styles/camperCard.module.css";
 import Form from "../components/Form.jsx";
 import star from "../assets/icons/Rating.svg";
+import name from "../assets/icons/name.svg";
 
 export default function Reviews({ camper }) {
   console.log("camper in reviews:", camper);
-
+  const index = camper.reviews.forEach((r) => {
+    return r.index;
+  });
   return (
     <>
       <div className={css.main}>
-        <div className={css.leftSide}>
+        <div className={css.RevLeftSide}>
           <div>
-            {camper.reviews.map((r) => {
+            {camper.reviews.map((r, index) => {
               return (
-                <li key={r.index}>
-                  <h4>{r.reviewer_name}</h4>
-                  <img src={star} />
-                  <p>{r.comment}</p>
+                <li key={index}>
+                  <div className={css.Rname}>
+                    <img src={name} className={css.image} />
+                    <p
+                      style={{
+                        fontWeight: "600px",
+                        fontStyle: "Semi Bold",
+                        fontSize: "24px",
+                        color: "var(--orange)",
+                      }}
+                      className={css.nameLetter}
+                    >
+                      {r.reviewer_name[0]}
+                    </p>
+                    <h2>{r.reviewer_name}</h2>
+                    {/* <img src={star}/> */}
+
+                    <div>
+                      <p>{r.comment}</p>
+                    </div>
+                  </div>
+
+                  <div></div>
                 </li>
               );
             })}

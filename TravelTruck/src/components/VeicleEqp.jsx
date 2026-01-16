b6e26ede4acd1b377753d98ae2cb3b1b9c3f80ee
Merve Delibaşoglu <mervedelibasoglu.dev@gmail.com>
2026-01-16 22:45:19 +0300

    butonları searcha ekleme

--- TravelTruck/src/components/VeicleEqp.jsx
+++ TravelTruck/src/components/VeicleEqp.jsx
@@ -5,30 +5,27 @@ import Automatic from "../public/assets/icons/Automatic.svg?react";
 import Kitchen from "../public/assets/icons/Kitchen.svg?react";
 import TV from "../public/assets/icons/TV.svg?react";
 import css from "../styles/equipments.module.css";
+
+
 export default function VeicleEqp() {
-  const icons = {
-    AC: AC,
-    Automatic: Automatic,
-    Kitchen: Kitchen,
-    TV: TV,
-    Bathroom: Bathroom,
-  };
+  const equipments = [
+    { name: "AC", Icon: AC },
+    { name: "Automatic", Icon: Automatic },
+    { name: "Kitchen", Icon: Kitchen },
+    { name: "TV", Icon: TV },
+    { name: "Bathroom", Icon: Bathroom },
+  ];
 
   return (
     <ul className={css.ul}>
-      {Object.entries(icons).map(([icon, iconSrc]) => {
-        const iconName = icon;
+      {equipments.map(({ name, Icon }) => {
         return (
-          <button className={css.listItem} key={iconName}>
-            <li >
-              <img
-                src={iconSrc}
-                alt="iconName"
-                style={{ width: "32px", height: "32px" }}
-              />
-              {iconName}
-            </li>
-          </button>
+          <li key={name}>
+            <button className={css.listItem} type="button">
+              <img src={Icon} style={{ width: "32px", height: "32px" }} />
+              <span> {name}</span>
+            </button>
+          </li>
         );
       })}
     </ul>
